1edf2f9e4673416c190e8fa5184f39d4612c6955
rodrigoo1604 <rodrigo.lastra.pescador@gmail.com>
2024-12-26 13:16:27 +0100

    feat: added controller with function

---
+++ app/Http/Controllers/DniController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DniLetter;
+use Illuminate\Http\Request;
+
+class DniController extends Controller
+{
+    public function calculateLetter(Request $request)
+    {
+        $validated = $request->validate([
+            'dni' => 'required|integer|between:0,99999999',
+        ]);
+
+        $dni = $validated['dni'];
+        $index = $dni % 23;
+
+        $letter = DniLetter::table('dni_letters')->where('index', $index)->value('letter');
+
+        if ($letter) {
+            return response()->json([
+                'dni' => $dni,
+                'letter' => $letter,
+            ]);
+        }
+
+        return response()->json(['error' => 'Letter cannot be calculated'], 500);
+    }
+}
